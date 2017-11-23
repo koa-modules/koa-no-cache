c813fbf1a07205d49ae3f330e64994bf194e9706
haoxin <haoxins@outlook.com>
2017-11-23 11:39:02 +0800

    lint

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-'use strict';
+'use strict'
 
-const pathToRegExp = require('path-to-regexp');
-const typeis = require('type-is').is;
+const pathToRegExp = require('path-to-regexp')
+const typeis = require('type-is').is
 
 function setNoCacheHeaders(ctx) {
-  ctx.set('Cache-Control', 'no-store, no-cache, must-revalidate');
-  ctx.set('Pragma', 'no-cache');
-  ctx.set('Expires', 0);
+  ctx.set('Cache-Control', 'no-store, no-cache, must-revalidate')
+  ctx.set('Pragma', 'no-cache')
+  ctx.set('Expires', 0)
 }
 
 module.exports = function noCache(options) {
-  options = options || {};
-  options.global = Boolean(options.global);
+  options = options || {}
+  options.global = Boolean(options.global)
 
-  const paths = options.paths || [];
-  const types = options.types || [];
+  const paths = options.paths || []
+  const types = options.types || []
   const config = options.config || {
     sensitive: true,
     strict: true
-  };
+  }
 
-  const pathsFinal = paths.map(path => pathToRegExp(path, [], config));
+  const pathsFinal = paths.map(path => pathToRegExp(path, [], config))
 
   if (options.global) {
     return async function noCacheForAll(ctx, next) {
-      await next();
-      setNoCacheHeaders(ctx);
-    };
+      await next()
+      setNoCacheHeaders(ctx)
+    }
   }
 
   // only load modules if needed
   return async function noCache(ctx, next) {
-    await next();
-      if (typeis(ctx.type, types) || pathsFinal.some(pathFinal => pathFinal.exec(ctx.path))) {
-        setNoCacheHeaders(ctx);
+    await next()
+    if (typeis(ctx.type, types) || pathsFinal.some(pathFinal => pathFinal.exec(ctx.path))) {
+      setNoCacheHeaders(ctx)
     }
-  };
-};
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,123 +1,123 @@
-'use strict';
+'use strict'
 
-const serve = require('koa-static-cache');
-const request = require('supertest');
-const assert = require('assert');
-const equal = assert.deepEqual;
-const noCache = require('..');
-const Koa = require('koa');
+const serve = require('koa-static-cache')
+const request = require('supertest')
+const assert = require('assert')
+const equal = assert.deepEqual
+const noCache = require('..')
+const Koa = require('koa')
 
-const globalNoCacheApp = new Koa();
-const app = new Koa();
+const globalNoCacheApp = new Koa()
+const app = new Koa()
 
 describe('# koa-no-cache', function() {
   app.use(noCache({
     paths: ['/api/status', '/users/(.*)'],
     types: ['manifest']
-  }));
-  app.use(serve('test'));
+  }))
+  app.use(serve('test'))
   app.use(async (ctx) => {
-      ctx.body = 'test';
-  });
+    ctx.body = 'test'
+  })
 
   globalNoCacheApp.use(noCache({
     global: true
-  }));
-  globalNoCacheApp.use(serve('test'));
+  }))
+  globalNoCacheApp.use(serve('test'))
   globalNoCacheApp.use(async (ctx) => {
-      ctx.body = 'test';
-  });
+    ctx.body = 'test'
+  })
 
   describe('types', function() {
     it('manifest', function(done) {
       request(app.listen())
         .get('/cache.manifest')
         .end(function(err, res) {
-          assertError(err);
-          assertRes(res, true);
-          done();
-        });
-    });
+          assertError(err)
+          assertRes(res, true)
+          done()
+        })
+    })
 
     it('javascript', function(done) {
       request(app.listen())
         .get('/index.js')
         .end(function(err, res) {
-          assertError(err);
-          assertRes(res, false);
-          done();
-        });
-    });
-  });
+          assertError(err)
+          assertRes(res, false)
+          done()
+        })
+    })
+  })
 
   describe('paths', function() {
     it('/api/status', function(done) {
       request(app.listen())
         .get('/api/status')
         .end(function(err, res) {
-          assertError(err);
-          assertRes(res, true);
-          done();
-        });
-    });
+          assertError(err)
+          assertRes(res, true)
+          done()
+        })
+    })
 
     it('/users/haoxin', function(done) {
       request(app.listen())
         .get('/users/haoxin')
         .end(function(err, res) {
-          assertError(err);
-          assertRes(res, true);
-          done();
-        });
-    });
+          assertError(err)
+          assertRes(res, true)
+          done()
+        })
+    })
 
     it('/cache', function(done) {
       request(app.listen())
         .get('/cache')
         .end(function(err, res) {
-          assertError(err);
-          assertRes(res, false);
-          done();
-        });
-    });
-  });
+          assertError(err)
+          assertRes(res, false)
+          done()
+        })
+    })
+  })
 
   describe('global', function() {
     it('manifest', function(done) {
       request(globalNoCacheApp.listen())
         .get('/cache.manifest')
         .end(function(err, res) {
-          assertError(err);
-          assertRes(res, true);
-          done();
-        });
-    });
+          assertError(err)
+          assertRes(res, true)
+          done()
+        })
+    })
 
     it('javascript', function(done) {
       request(globalNoCacheApp.listen())
         .get('/index.js')
         .end(function(err, res) {
-          assertError(err);
-          assertRes(res, true);
-          done();
-        });
-    });
-  });
-});
+          assertError(err)
+          assertRes(res, true)
+          done()
+        })
+    })
+  })
+})
 
 function assertError(e) {
-  if (e) console.error(e);
+  if (e) console.error(e)
 
-  assert(!e, 'error exist');
+  assert(!e, 'error exist')
 }
 
 function assertRes(res, noCache) {
-  equal(res.status, 200);
-  assert(res.text);
+  equal(res.status, 200)
+  assert(res.text)
 
   if (noCache) {
-    equal(res.headers['cache-control'], 'no-store, no-cache, must-revalidate');
-    equal(res.headers.pragma, 'no-cache');
-    equal(res.headers.expires, '0');
+    equal(res.headers['cache-control'], 'no-store, no-cache, must-revalidate')
+    equal(res.headers.pragma, 'no-cache')
+    equal(res.headers.expires, '0')
   }
 }
